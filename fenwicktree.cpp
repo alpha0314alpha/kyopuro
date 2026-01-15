9150b0c0f8bbe4f1717d2fe80cbae152316163ce
alpha0314alpha <alpha0314alpha@gmail.com>
2026-01-15 17:47:41 +0900

    Add Fenwick Tree implementation in C++
    Implement a Fenwick Tree (Binary Indexed Tree) class with methods for adding values and calculating prefix and range sums.

---
+++ fenwicktree.cpp
@@ -0,0 +1,29 @@
+template<typename T> class fenwicktree{
+private:
+    int n;
+    vector<T> bit;
+
+public:
+    fenwicktree(int n) : n(n), bit(n, T()) {}
+
+    void add(int i, T x){
+        while(i < n){
+            bit[i] += x;
+            i |= i+1;
+        }
+    }
+
+    T sum(int i) const{
+        T res = T();
+        while(i >= 0){
+            res += bit[i];
+            i = (i & (i + 1)) - 1;
+        }
+        return res;
+    }
+
+    T range_sum(int l, int r) const{
+        if(l > r) return T();
+        return sum(r)-(l? sum(l-1): T());
+    }
+};
