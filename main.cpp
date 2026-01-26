5813165676aa80a3cad5269afded32947543e35a
alpha0314alpha <alpha0314alpha@gmail.com>
2026-01-26 23:13:35 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
-#define INF (1ll << 60)
-#define INFLL (ll)(4e18)
+#define INF (1ll<<60)
 // #define MOD 998244353
 #define MOD 1000000007
+#define EPS 1e-14
 #define PI acosl(-1)
 #define lb lower_bound
 #define ub upper_bound
@@ -12,6 +12,9 @@
 #define nextp next_permutation
 #define maxe max_element
 #define mine min_element
+#define accu accumulate
+#define popc __builtin_popcount
+#define elif else if 
 #define nall(A) A.begin(), A.end()
 #define rall(A) A.rbegin(), A.rend()
 #define rep(i, n) for(int i = 0; i < (n); i++) 
@@ -21,22 +24,30 @@
 #define yes cout<<"Yes"<<endl
 #define no cout<<"No"<<endl
 #define yesno(a) cout<<(a?"Yes":"No")<<endl
+#define sanko(a, b, c) (a? b: c)
 using namespace std;
 using ll = long long;
 using ull = unsigned long long;
 using ld = long double;
 using pii = pair<int, int>;
-template<class T> using pq = priority_queue<T>;
-template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+template<typename T> using pq = priority_queue<T>;
+template<typename T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+template<typename T> using vv = vector<vector<T>>;
 const int dx[] = {1, 0, -1, 0};
 const int dy[] = {0, -1, 0, 1};
 const int dx8[] = { 1, 1, 0, -1, -1, -1, 0, 1 };
 const int dy8[] = { 0, -1, -1, -1, 0, 1, 1, 1 };
+const string abcdefghijklmnopqrstuvwxyz = "abcdefghijklmnopqrstuvwxyz";
+const string ABCDEFGHIJKLMNOPQRSTUVWXYZ = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 template<typename T> bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
 template<typename T> bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
-template<typename T> T nC2(T n) { return (n*n-n)/2; }
-template<typename T> T nC3(T n) { return n*(n-1)*(n-2)/6; }
-template<typename T> T fact(T n) { ll res = 1; for (ll i = 1; i <= n; i++) res *= i; return res; }
+template<typename T> T nc2(T n){ return n*(n-1)/2; }
+
+template<typename T> istream& operator>>(istream& is, vector<T>& v) {
+    for (T& x : v) is >> x;
+    return is;
+}
+
 vector<pair<char, int>> to_rle(const string& S) {
 	vector<pair<char, int>> res;
 	for (char i : S) {
@@ -45,6 +56,7 @@ vector<pair<char, int>> to_rle(const string& S) {
 	}
 	return res;
 }
+
 template<typename T> vector<pair<T, int>> to_rle(const vector<T>& A){
     vector<pair<T, int>> res;
     for (T i : A){
@@ -53,6 +65,7 @@ template<typename T> vector<pair<T, int>> to_rle(const vector<T>& A){
     }
     return res;
 }
+
 template<typename T> vector<int> zaatu(const vector<T>& A){
     int N = (int)A.size();
     vector<T> B = A;
@@ -65,12 +78,14 @@ template<typename T> vector<int> zaatu(const vector<T>& A){
 
     return res;
 }
+
 template<typename T> void output(const vector<T>& A, bool ln){
     int N = (int)A.size();
     for (int i = 0; i < N; i++){
         cout << A[i] << (ln || i == N-1? '\n': ' ');
     }
 }
+
 template<typename T> map<T, int> prime_factor(T N){
     map<T, int> res;
     for (T i = 2; i*i <= N && N > 1; i++){
@@ -82,7 +97,8 @@ template<typename T> map<T, int> prime_factor(T N){
     if (N > 1) res[N]++;
     return res;
 }
-ll modpow(ll a, ll e, ll mod){
+
+ll modpow(ll a, ll e, ll mod = MOD){
     if (e == 0) return 1;
     ll res = 1;
     while (e){
@@ -92,6 +108,13 @@ ll modpow(ll a, ll e, ll mod){
     }
     return res;
 }
+
+bool iskaibun(string s){
+    string t = s;
+    reverse(t.begin(), t.end());
+    return s == t;
+}
+
 class dsu{
 private:
     vector<int> parent;
@@ -126,6 +149,86 @@ public:
         return sz[root(x)];
     }
 };
+template<typename T> class wdsu{
+private:
+    vector<int> parent, rankv;
+    vector<T> weight;
+public:
+    wdsu(int n){
+        parent.resize(n);
+        rankv.assign(n, 0);
+        weight.assign(n, 0);
+        for (int i = 0; i < n; i++) parent[i] = i;
+    }
+
+    int root(int x){
+        if (parent[x] == x) return x;
+        int r = root(parent[x]);
+        weight[x] += weight[parent[x]];
+        return parent[x] = r;
+    }
+
+    T potential(int x){
+        root(x);
+        return weight[x];
+    }
+
+    bool merge(int x, int y, T w){
+        int rx = root(x), ry = root(y);
+        if (rx == ry) return false;
+        if (rankv[rx] < rankv[ry]){
+            swap(rx, ry);
+            swap(x, y);
+            w = -w;
+        }
+        parent[ry] = rx;
+        weight[ry] = weight[x]+w-weight[y];
+        if (rankv[rx] == rankv[ry]) rankv[rx]++;
+        return true;
+    }
+
+    T diff(int x, int y){
+        return potential(y)-potential(x);
+    }
+
+    bool same(int x, int y){
+        return root(x) == root(y);
+    }
+};
+template<typename T> class fenwicktree{
+private:
+    int n;
+    vector<T> bit, a;
+
+public:
+    fenwicktree(int n) : n(n), bit(n, T()), a(n, T()) {}
+
+    void add(int i, T x){
+        while(i < n){
+            bit[i] += x;
+            i |= i + 1;
+        }
+    }
+
+    void set(int i, T x){
+        add(i, x - a[i]);
+        a[i] = x;
+    }
+
+    T sum(int i) const{
+        T res = T();
+        while(i >= 0){
+            res += bit[i];
+            i = (i&(i+1))-1;
+        }
+        return res;
+    }
+
+    T range_sum(int l, int r) const{
+        if(l > r) return T();
+        return sum(r)-(l? sum(l-1): T());
+    }
+};
 template<class T> class segtree{
 private:
     int n;
@@ -151,6 +254,24 @@ private:
         return op(query(node*2, l, mid, ql, qr), query(node*2+1, mid, r, ql, qr));
     }
 
+    int find_first(int node, int nl, int nr, int l, T val){
+        if (nr <= l) return -1;
+        if (seg[node] < val) return -1;
+        if (nr-nl == 1) return nl;
+
+        int mid = (nl+nr)/2;
+        int res = -1;
+
+        if (l < mid){
+            res = find_first(node*2, nl, mid, l, val);
+            if (res != -1) return res;
+            return find_first(node*2+1, mid, nr, l, val);
+        }
+        else{
+            return find_first(node*2+1, mid, nr, l, val);
+        }
+    }
+
     void update(int node, int l, int r, int idx, T val){
         if (r-l == 1){
             seg[node] = val;
@@ -160,6 +281,13 @@ private:
         if (idx < mid) update(node*2, l, mid, idx, val);
         else update(node*2+1, mid, r, idx, val);
         seg[node] = op(seg[node*2], seg[node*2+1]);
+    }
+
+    T get(int node, int l, int r, int idx) const{
+        if (r-l == 1) return seg[node];
+        int mid = (l+r)/2;
+        if (idx < mid) return get(node*2, l, mid, idx);
+        else return get(node*2+1, mid, r, idx);
     }
 public:
     segtree(const vector<T>& A, T id, function<T(T, T)> op) : e(id), op(op){
@@ -175,53 +303,69 @@ public:
     void update(int idx, T val){
         update(1, 0, n, idx, val);
     }
+
+    int find_first(int l, T val){
+        return find_first(1, 0, n, l, val);
+    }
+
+    T get(int idx) const{
+        return get(1, 0, n, idx);
+    }
+
+    void add(int idx, T val){
+        update(idx, get(idx) + val);
+    }
 };
 template<class T> class lazy_segtree {
 private:
     int n;
     vector<T> seg, lazy;
+    vector<bool> flag;
     T e;
     function<T(T, T)> op;
 
     void build(int node, int l, int r, const vector<T>& A){
-        if (r - l == 1) {
+        if (r-l == 1){
             seg[node] = A[l];
             return;
         }
-        int mid = (l + r) / 2;
+        int mid = (l+r)/2;
         build(node*2, l, mid, A);
         build(node*2+1, mid, r, A);
         seg[node] = op(seg[node*2], seg[node*2+1]);
     }
 
-    void push(int node){
-        if (lazy[node] != 0) {
-            seg[node*2] += lazy[node];
-            seg[node*2+1] += lazy[node];
-            lazy[node*2] += lazy[node];
-            lazy[node*2+1] += lazy[node];
-            lazy[node] = 0;
-        }
+    void push(int node, int l, int r){
+        if (!flag[node]) return;
+        int mid = (l+r)/2;
+        apply(node*2, l, mid, lazy[node]);
+        apply(node*2+1, mid, r, lazy[node]);
+        flag[node] = false;
+    }
+
+    void apply(int node, int l, int r, T val){
+        seg[node] = val*(r-l);
+        lazy[node] = val;
+        flag[node] = true;
     }
 
     void update(int node, int l, int r, int ql, int qr, T val){
         if (qr <= l || r <= ql) return;
-        if (ql <= l && r <= qr) {
-            seg[node] += val;
-            lazy[node] += val;
+        if (ql <= l && r <= qr){
+            apply(node, l, r, val);
             return;
         }
-        push(node);
+        push(node, l, r);
         int mid = (l+r)/2;
-        update(node * 2, l, mid, ql, qr, val);
-        update(node * 2 + 1, mid, r, ql, qr, val);
+        update(node*2, l, mid, ql, qr, val);
+        update(node*2+1, mid, r, ql, qr, val);
         seg[node] = op(seg[node*2], seg[node*2+1]);
     }
 
     T query(int node, int l, int r, int ql, int qr){
         if (qr <= l || r <= ql) return e;
         if (ql <= l && r <= qr) return seg[node];
-        push(node);
+        push(node, l, r);
         int mid = (l+r)/2;
         return op(
             query(node*2, l, mid, ql, qr),
@@ -230,10 +374,11 @@ private:
     }
 
 public:
-    lazy_segtree(const vector<T>& A, T id, function<T(T, T)> op) : e(id), op(op){
+    lazy_segtree(const vector<T>& A, T id, function<T(T,T)> op) : e(id), op(op){
         n = A.size();
         seg.assign(4*n, e);
         lazy.assign(4*n, 0);
+        flag.assign(4*n, false);
         build(1, 0, n, A);
     }
 
@@ -312,11 +457,160 @@ public:
         return depth[u]+depth[v]-2*depth[w];
     }
 };
+class comb{
+private:
+    vector<ll> fact, invfact;
+    ll mod;
+    int size;
+public:
+    comb(int n, ll m){
+        mod = m, size = n;
+        fact.resize(size+1);
+        invfact.resize(size+1);
+        for (int i = 0; i <= size; i++){
+            if (i == 0) fact[i] = 1;
+            else fact[i] = fact[i-1]*i%mod;
+        }
+        invfact[n] = modpow(fact[n], mod-2, mod);
+        for (int i = size; i >= 1; i--){
+            invfact[i-1] = invfact[i]*i%mod;
+        }
+    }
+
+    ll factorial(int n){
+        if (n > size) return -1;
+        return fact[n];
+    }
+
+    ll npr(int n, int r){
+        if (r < 0 || r > n || n > size) return 0;
+        return fact[n]*invfact[n-r]%mod;
+    }
+
+    ll ncr(int n, int r){
+        if (r < 0 || r > n || n > size) return 0;
+        return fact[n]*invfact[r]%mod*invfact[n-r]%mod;
+    }
+
+    ll nhr(int n, int r){
+        if (r < 0 || r > n || n > size) return 0;
+        return ncr(n+r-1, r);
+    }
+};
+class scc{
+public:
+    int N;
+    vector<vector<int>> graph;
+    vector<int> ord, low, comp;
+    vector<bool> in_stack;
+    stack<int> st;
+    int noword = 0, compcount = 0;
+
+    scc(int n) : N(n), graph(n) {}
+
+    void add_edge(int u, int v){
+        graph[u].push_back(v);
+    }
+
+    void dfs(int v){
+        noword++;
+        ord[v] = noword;
+        low[v] = noword;
+        st.push(v);
+        in_stack[v] = true;
+        for (int to : graph[v]){
+            if (ord[to] == -1){
+                dfs(to);
+                low[v] = min(low[v], low[to]);
+            }
+            else if (in_stack[to]){
+                low[v] = min(low[v], ord[to]);
+            }
+        }
+        if (ord[v] == low[v]){
+            while (true){
+                int u = st.top();
+                st.pop();
+                in_stack[u] = false;
+                comp[u] = compcount;
+                if (u == v) break;
+            }
+            compcount++;
+        }
+    }
+
+    int build(){
+        ord.assign(N, -1);
+        low.resize(N);
+        comp.resize(N);
+        in_stack.assign(N, false);
+        for (int i = 0; i < N; i++){
+            if (ord[i] == -1) dfs(i);
+        }
+        return compcount;
+    }
+};
+class modint{
+public:
+    static const int mod = 998244353;
+    int v;
+    
+    modint(ll _v = 0){
+        v = int((_v%mod+mod)%mod);
+    }
+
+    modint& operator += (const modint& o){
+        v += o.v;
+        if (v >= mod) v -= mod;
+        return *this;
+    }
+
+    modint operator -= (const modint& o){
+        v -= o.v;
+        if (v < 0) v += mod;
+        return *this;
+    }
+
+    modint& operator *= (const modint& o){
+        v = (ll)v*o.v%mod;
+        return *this;
+    }
+
+    friend modint operator + (modint a, const modint& b){
+        return a += b;
+    }
+
+    friend modint operator - (modint a, const modint& b){
+        return a -= b;
+    }
+
+    friend modint operator * (modint a, const modint& b){
+        return a *= b;
+    }
+
+    modint pow(ll n) const {
+        modint res = 1, a = *this;
+        while (n){
+            if (n&1) res *= a;
+            a *= a;
+            n >>= 1;
+        }
+        return res;
+    }
+
+    modint inv() const{
+        return pow(MOD-2);
+    }
+
+    friend modint operator / (modint a, const modint& b){
+        return a *= b.inv();
+    }
+};
 
 #ifndef ONLINE_JUDGE
-signed limit = 10;
+signed testcases = 10;
 #else
-signed limit = 1;
+signed testcases = 1;
 #endif
 #define int long long
 
@@ -324,14 +618,18 @@ signed limit = 1;
 signed main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    // cin >> limit;
+    // cin >> testcases;
 
-    while (limit--){
+    while (testcases--){
     }
 }
 #else
 signed main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    // cin >> testcases;
+
+    while (testcases--){
+    }
 }
 #endif
