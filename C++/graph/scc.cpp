988760ffd366b6195bbc6a3a458ce84d1b236fce
alpha <alpha0314alpha@gmail.com>
2026-04-12 21:53:46 +0900

    scc

--- C++/graph/scc.cpp
+++ C++/graph/scc.cpp
@@ -1,54 +1,34 @@
-class scc{
-public:
+struct scc{
     int N;
-    vector<vector<int>> graph;
-    vector<int> ord, low, comp;
-    vector<bool> in_stack;
-    stack<int> st;
-    int noword = 0, compcount = 0;
+    vector<vector<int>> G, RG;
+    vector<int> comp, order;
+    vector<bool> used;
 
-    scc(int n) : N(n), graph(n) {}
+    scc(int n) : N(n), G(n), RG(n), comp(n, -1), used(n, false) {}
 
     void add_edge(int u, int v){
-        graph[u].push_back(v);
+        G[u].push_back(v);
+        RG[v].push_back(u);
     }
 
-    void dfs(int v){
-        noword++;
-        ord[v] = noword;
-        low[v] = noword;
-        st.push(v);
-        in_stack[v] = true;
-        for (int to : graph[v]){
-            if (ord[to] == -1){
-                dfs(to);
-                low[v] = min(low[v], low[to]);
-            }
-            else if (in_stack[to]){
-                low[v] = min(low[v], ord[to]);
-            }
-        }
-        if (ord[v] == low[v]){
-            while (true){
-                int u = st.top();
-                st.pop();
-                in_stack[u] = false;
-                comp[u] = compcount;
-                if (u == v) break;
-            }
-            compcount++;
-        }
+    void dfs(int n){
+        used[n] = true;
+        for (int i : G[n]) if (!used[i]) dfs(i);
+        order.push_back(n);
     }
 
-    int build(){
-        noword = 0, compcount = 0;
-        ord.assign(N, -1);
-        low.resize(N);
-        comp.resize(N);
-        in_stack.assign(N, false);
-        for (int i = 0; i < N; i++){
-            if (ord[i] == -1) dfs(i);
-        }
-        return compcount;
+    void rdfs(int n, int k){
+        comp[n] = k;
+        for (int i : RG[n]) if (comp[i] == -1) rdfs(i, k);
+    }
+
+    vector<vector<int>> build(){
+        for (int i = 0; i < N; i++) if (!used[i]) dfs(i);
+        reverse(order.begin(), order.end());
+        int k = 0;
+        for (int i : order) if (comp[i] == -1) rdfs(i, k++);
+        vector<vector<int>> res(k);
+        for (int i = 0; i < N; i++) res[comp[i]].push_back(i);
+        return res;
     }
 };
