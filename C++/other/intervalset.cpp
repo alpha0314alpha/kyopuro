bf16a6563dcf381aa3960c9baa506f689fa77a69
alpha0314alpha <alpha0314alpha@gmail.com>
2026-05-08 08:23:33 +0900

    Refactor intervalset class and update methods

--- C++/other/intervalset.cpp
+++ C++/other/intervalset.cpp
@@ -1,46 +1,88 @@
-template<typename T> class intervalset{
-public:
-    set<pair<T, T>> s;
+template<typename T> struct intervalset {
+    static constexpr T inf = numeric_limits<T>::max();
+    static constexpr T ninf = numeric_limits<T>::lowest();
+    set<pair<T,T>> st;
 
-    void add(T l, T r){
-        auto it = s.lower_bound({l, numeric_limits<T>::lowest()});
-        if (it != s.begin()) it--;
-        while (it != s.end()){
-            if (it->second < l){
-                it++;
-                continue;
-            }
-            if (it->first > r) break;
-            l = min(l, it->first);
-            r = max(r, it->second);
-            it = s.erase(it);
-        }
-        s.insert({l, r});
+    bool touch_left(T a, T b) const{
+        return b == inf || a <= b + 1;
     }
 
-    void remove(T l, T r){
-        auto it = s.lower_bound({l+1, numeric_limits<T>::lowest()});
-        if (it != s.begin()) it--;
-        vector<pair<T, T>> add;
-        while (it != s.end()){
-            T L = it->first;
-            T R = it->second;
+    bool touch_right(T a, T b) const{
+        return b == ninf || a >= b - 1;
+    }
+
+    auto find(T x){
+        auto it = st.upper_bound({x, inf});
+        if (it == st.begin()) return st.end();
+        --it;
+        if (it->first <= x && x <= it->second) return it;
+        return st.end();
+    }
+
+    bool contains(T x){
+        return find(x) != st.end();
+    }
+
+    bool contains(T l, T r){
+        if (l > r) return false;
+        auto it = find(l);
+        return it != st.end() && r <= it->second;
+    }
+
+    void insert(T l, T r){
+        if (l > r) return;
+        auto it = st.lower_bound({l, ninf});
+        if (it != st.begin()){
+            auto prv = prev(it);
+            if (touch_right(prv->second, l)) it = prv;
+        }
+        while (it != st.end() && touch_left(it->first, r)){
+            l = min(l, it->first);
+            r = max(r, it->second);
+            it = st.erase(it);
+        }
+        st.insert({l, r});
+    }
+
+    void erase(T l, T r){
+        if (l > r) return;
+        auto it = st.lower_bound({l, ninf});
+        if (it != st.begin()) --it;
+        vector<pair<T,T>> add;
+        while (it != st.end()){
+            auto [L, R] = *it;
             if (R < l){
-                it++;
+                ++it;
                 continue;
             }
             if (L > r) break;
-            if (L < l) add.push_back({L, l-1});
-            if (R > r) add.push_back({r+1, R});
-            it = s.erase(it);
+            it = st.erase(it);
+            if (L < l) add.push_back({L, T(l-1)});
+            if (r < R) add.push_back({T(r+1), R});
         }
-
-        for (auto p : add) s.insert(p);
+        for (auto &p : add) st.insert(p);
     }
 
-    void print(){
-        for (auto p : s){
-            cout << '[' << p.first << ',' << p.second << ']' << endl;
-        }
+    void insert(T x){
+        insert(x, x);
+    }
+
+    void erase(T x){
+        erase(x, x);
+    }
+
+    vector<pair<T, T>> intervals() const{
+        return vector<pair<T, T>>(st.begin(), st.end());
+    }
+
+    T mex(T x){
+        auto it = find(x);
+        if (it == st.end()) return x;
+        if (it->second == inf) return inf;
+        return it->second + 1;
+    }
+
+    int size() const{
+        return (int)st.size();
     }
 };
