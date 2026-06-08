0fd162fd3ce137f85cb4431da96976b5ac0003ea
alpha0314alpha <alpha0314alpha@gmail.com>
2026-06-08 22:58:43 +0900

    const

---
+++ all.py
@@ -0,0 +1,36 @@
+from acl_cpp.convolution import convolution
+from acl_cpp.dsu import dsu
+from acl_cpp.fenwicktree import fenwick_tree as fenwicktree
+from acl_cpp.maxflow import mf_graph
+from acl_cpp.mincostflow import mcf_graph
+from acl_cpp.scc import scc_graph
+from acl_cpp.twosat import two_sat as twosat
+from acl_cpp.string import *
+from acl_cpp.math import *
+from acl_cpp.internal.math import *
+from atcoder.segtree import SegTree as segtree
+from atcoder.lazysegtree import LazySegTree as lazy_segtree
+from sortedcontainers import *
+from collections import *
+import heapq
+import bisect
+import sys
+import numpy as np
+import random
+import math
+
+sys.setrecursionlimit(10**8)
+input = sys.stdin.readline
+lb = bisect.bisect_left
+ub = bisect.bisect_right
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+dxy = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+PI = math.pi
+EPS = 1e-14
+INF = 1<<60
+MOD = 998244353
+# MOD = 10**9+7
+
+if __name__ == "__main__":
+    pass
